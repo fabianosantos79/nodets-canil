663c757e24f39cb230373446bf05abb68c5bbe7e
Fabiano <fabiano.ssantos79@gmail.com>
2022-12-18 05:32:59 -0300

    cls

--- src/controllers/searchController.ts
+++ src/controllers/searchController.ts
@@ -1,5 +1,21 @@
 import { Request, Response } from "express";
 
+import { Pets } from '../models/Pets';
+import { createMenuObject } from '../helpers/createMenuObject';
+
 export const search = (req: Request, res: Response) => {
-    //res.render('pages/search');
+    let query: string = req.query.busca as string;
+
+    if(!query){
+        res.redirect('/');
+        return;
+    }
+
+    let list = Pets.getFromName(query);
+
+    res.render('pages/page', {
+        menu: createMenuObject(''),
+        list,
+        query
+    });
 }
---
+++ src/helpers/createMenuObject.ts
@@ -0,0 +1,16 @@
+type menuOptions = '' | 'all' | 'dogs' | 'cats' | 'fishes';
+
+export const createMenuObject = (activeMenu: menuOptions) => {
+    let returnObject = {
+        all: false,
+        dogs: false,
+        cats: false,
+        fishes: false
+    }
+
+    if(activeMenu !== ''){
+        returnObject[activeMenu] = true;
+    }
+
+    return returnObject;
+};
